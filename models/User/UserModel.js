34b0198f168f7b2113725a21a112ccd5579ffe50
Rahul7874 <rp158835@gmail.com>
2023-11-27 18:01:36 +0530

    deleted

--- models/User/UserModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    Username: { type: String, default: "" },
-    Password: { type: String, default: ""},
-});
-
-const User = mongoose.model('User', userSchema);
-module.exports = User;
--- routes/User/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const registerUser = require("../../controllers/User/UserController.js");
-
-const router = express.Router();
-
-router.post('/registeruser',registerUser);
-
-module.exports = router;
